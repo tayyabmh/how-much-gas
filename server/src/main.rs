1db507bb0d47051e77d3c90cd74a52b80ce63082
tayyab <tayyab@tayyabs-MacBook-Pro.local>
2021-11-30 11:06:08 -0500

    added gas computation logic

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,16 +6,28 @@ use actix_web::{get, web, post, App, HttpResponse, HttpServer, Responder, Result
 use dotenv::dotenv;
 use listenfd::ListenFd;
 use std::env;
-use serde_json::json;
 use serde::{Deserialize, Serialize};
 use chrono::Utc;
 use std::collections::HashMap;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 struct GasCalcObject {
     address: String,
     time_period: String
 }
+
+#[derive(Deserialize, Serialize, Debug)]
+struct GasUsed {
+    gas_used: u32
+}
+
+#[derive(Deserialize, Debug)]
+struct TransactionList {
+    status: String,
+    message: String,
+    result: Vec<HashMap<String, String>>
+}
+
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -35,7 +47,7 @@ async fn get_block_number_from_timestamp(timestamp: i64) -> Result<String, Box<d
 }
 
 #[tokio::main]
-async fn get_list_of_transactions_by_address(address: String, startblock: u32, endblock: u32) -> Result<(), Box<dyn std::error::Error>> {
+async fn get_list_of_transactions_by_address(address: String, startblock: u32, endblock: u32) -> Result<u32, Box<dyn std::error::Error>> {
     let url = format!(
         "https://api.etherscan.io/api?module=account&action=txlist&address={}&startblock={}&endblock={}&apikey={}",
         address,
@@ -44,22 +56,35 @@ async fn get_list_of_transactions_by_address(address: String, startblock: u32, e
         env::var("APIKEY").expect("API Key not set")
     );
 
-    let response = reqwest::get(url)
+    let response: TransactionList = reqwest::get(url)
         .await?
-        .json::<HashMap<String,String>>()
+        .json()
         .await?;
 
-    Ok(())
+    println!("Response: {:?}", response.result);
+
+    let mut gas_count = 0;
+    for txn in response.result {
+        if txn["from"].to_uppercase() == address.to_uppercase() {
+            let gas_used = txn["gasUsed"].to_string().parse::<u32>().unwrap();
+            gas_count += gas_used;
+        }
+    }
+    
+    println!("Total Gas Used: {:?}", gas_count);
+    Ok(gas_count)
 
 }
 
 #[post("/calculate")]
-async fn calculate_gas_fees(request_body: web::Json<GasCalcObject>) -> Result<String> {
+async fn calculate_gas_fees(request_body: web::Json<GasCalcObject>) -> HttpResponse {
     let dt = Utc::now();
     let timestamp = dt.timestamp();
     let time_range;
-
-    if(request_body.time_period.as_str() != "AllTime") {
+    let mut total_gas_used: u32 = 0;
+    let end_block_number: u32 = get_block_number_from_timestamp(timestamp).unwrap().parse().unwrap();
+    let start_block_number: u32;
+    if request_body.time_period.as_str() != "AllTime" {
         match request_body.time_period.as_str() {
             "Last24Hours" => time_range = 86400,
             "Last7Days" => time_range = 604800,
@@ -71,18 +96,19 @@ async fn calculate_gas_fees(request_body: web::Json<GasCalcObject>) -> Result<St
         }
         
         let start_timestamp = timestamp - time_range;
-        println!("{:?}", start_timestamp);
-        let end_block_number: u32 = get_block_number_from_timestamp(timestamp).unwrap().parse().unwrap();
-        let start_block_number: u32 = get_block_number_from_timestamp(start_timestamp).unwrap().parse().unwrap();
-        println!("End block: {:?}", end_block_number);
-        println!("Start block: {:?}", start_block_number);
+        
+        start_block_number = get_block_number_from_timestamp(start_timestamp).unwrap().parse().unwrap();
+        
     } else {
-        let start_block_number = 0;
+        start_block_number = 0;
     }
-    
+    total_gas_used = get_list_of_transactions_by_address(request_body.address.to_string(), start_block_number, end_block_number).unwrap();
 
+    // I need to convert the gas_used to a number in Gwei or ethereum
 
-    Ok(format!("Address: {}, Time Period: {}, current_time: {}", request_body.address, request_body.time_period, timestamp))
+    HttpResponse::Ok().json(GasUsed {
+        gas_used: total_gas_used
+    })
 }
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
